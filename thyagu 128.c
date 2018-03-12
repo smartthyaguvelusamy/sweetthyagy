2a0b478dfadda472ca155533bb64f28597820417
smartthyaguvelusamy <35221750+smartthyaguvelusamy@users.noreply.github.com>
2018-03-12 16:50:15 +0530

    Create thyagu 128.c

---
+++ thyagu 128.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int num,a[50],sum=0,avg,i;
+printf("enter the limit");
+scanf("%d",&num);
+printf("enter the number");
+for(i=0;i<num;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("avg is");
+for(i=0;i<n;i++)
+{
+sum=sum+a[i];
+}
+avg=sum/num;
+printf("%d",avg);
+}
