f40183acf2271738ec4492a0d3799ded25acc5c9
smartthyaguvelusamy <35221750+smartthyaguvelusamy@users.noreply.github.com>
2018-03-19 16:16:21 +0530

    Create thyagu38.c

---
+++ thyagu38.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+int main(void) 
+{
+int n;
+scanf("%d",&n);
+switch(n)
+{
+case 1:
+printf("One \n");
+break;
+case 2:
+printf("Two\n");
+break;
+case 3:
+printf("Three\n");
+break;
+case 4:
+printf("Four\n");
+break;
+case 5:
+printf("Five\n");
+break;
+case 6:
+printf("Six\n ");
+break;
+case 7:
+printf("Seven\n ");
+break;
+case 8:
+printf("Eight\n");
+break;
+case 9:
+printf("Nine\n");
+break;
+case 10:
+printf("Ten\n");
+break;
+default:
+printf("Enter number between 1 and 10");
+}
+return 0;
+}
