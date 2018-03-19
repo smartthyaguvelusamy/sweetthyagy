a33b02b8490e29ad843548dbaf96c6691d5f763a
smartthyaguvelusamy <35221750+smartthyaguvelusamy@users.noreply.github.com>
2018-03-19 16:33:24 +0530

    Create thyagu77.c

---
+++ thyagu77.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+    int t[50],t1,i;
+    char bn[50],s1[50];
+    printf("Bus timetable system \n");
+     printf("enter busname :\n");
+    for(i=0;i<=4;i++)
+    {
+        scanf("%[^\n]s\n",bn[i]);
+    }
+    printf("enter the route:\n");
+    for(i=0;i<=4;i++)
+    {
+    scanf("%[^\n]s\n",s1[i]);
+}
+printf("enter the time:");
+for(i=0;i<=4;i++)
+{
+    scanf("%[^\n]d",t[i]);
+ 
+}
+for(i=0;i<=4;i++){
+printf("%s %s  %d",bn[i],s1[i],t[i]);
+}
+}
